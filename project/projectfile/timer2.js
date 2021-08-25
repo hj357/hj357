96b703f3f67911fa2b17706f57ca0d1f295b462c
hj357 <88478762+hj357@users.noreply.github.com>
2021-08-26 04:52:20 +0900

    Update timer2.js

--- project/projectfile/timer2.js
+++ project/projectfile/timer2.js
@@ -10,7 +10,6 @@ document.querySelector("#describe").style.display = "none";
 document.querySelector("#open").style.display = "block";
 }
 
-// Audio files
 var focusSound = new Audio("projectfile/focusbuzzer.mp3");
 var breakSound  = new Audio("projectfile/breakbuzzer.mp3");
 
@@ -19,7 +18,6 @@ var breakSound  = new Audio("projectfile/breakbuzzer.mp3");
 var session_seconds = "00";
 var session_minutes = 25;
 
-
 // Starting template for the timer
 function template() {
   document.getElementById("minutes").innerHTML = session_minutes;
@@ -27,78 +25,61 @@ function template() {
 }
 
 
-function reset(){
-    document.getElementById("done").style.display = "none";
-    start_timer();
-}
 
 
 function start_timer() {
   focusSound.play();
 
-  // Change the minutes and seconds to starting time
   session_minutes = 24;
   session_seconds = 59;
 
-  // Add the seconds and minutes to the page
   document.getElementById("minutes").innerHTML = session_minutes;
   document.getElementById("seconds").innerHTML = session_seconds;
 
-  // Start the countdown
   var minutes_interval = setInterval(minutesTimer, 60000);
   var seconds_interval = setInterval(secondsTimer, 1000);
 
+  document.getElementById("done").innerHTML =
+  "FOCUS!";
 
-  // Functions
-  // Function for minute counter
+  document.getElementById("done").classList.add("show_message");
+
   function minutesTimer() {
     session_minutes = session_minutes - 1;
     document.getElementById("minutes").innerHTML = session_minutes;
   }
 
 
-  // Function for second counter
   function secondsTimer() {
     session_seconds = session_seconds - 1;
     document.getElementById("seconds").innerHTML = session_seconds;
 
-    // Check if the seconds and minutes counter has reached 0
-    // If reached 0 then end the session
     if (session_seconds <= 0) {
       if (session_minutes <= 0) {
-        // Clears the interval i.e. stops the counter
+
         clearInterval(minutes_interval);
         clearInterval(seconds_interval);
 
-        // Add the message to the html
         document.getElementById("done").innerHTML =
           "Take a Break!";
-
-        // Make the html message div visible
         document.getElementById("done").classList.add("show_message");
 
-        // PLay the bell sound to tell the end of session
         breakSound.play();  
         breakTimer();
-
       }
-
       // Reset the session seconds to 60
       session_seconds = parseInt(60);
- 
     }
   function breakTimer(){
     focusSound.play();
 
-    // Change the minutes and seconds to starting time
     session_minutes = 4;
     session_seconds = 60;
   
-    // Add the seconds and minutes to the page
+
     document.getElementById("minutes").innerHTML = session_minutes;
     document.getElementById("seconds").innerHTML = session_seconds;
   
-  // Start the countdown
   var minutes_interval = setInterval(minutesTimer, 60000);
   var seconds_interval2 = setInterval(secondsTimer2, 1000);    
 
@@ -116,16 +97,13 @@ function start_timer() {
         document.getElementById("done").innerHTML =
           "Session Completed!!";
 
-        // Make the html message div visible
         document.getElementById("done").classList.add("show_message");
 
-        // PLay the bell sound to tell the end of session
         breakSound.play();  
       }
       session_seconds = parseInt(60);
-}
-
-}
-}
+    }
+    }
+    }
 }
 }
